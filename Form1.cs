aafe133d5a95b5fb2a11063a5f965d6b6600a620
이기주 <leegiju012@gmail.com>
2026-03-12 12:33:40 +0900

    3단계 과제: 현재 점수를 클래스 창에 저장하고 MouseEnter일 때 점수를 감소시키고 Button_click일 때 점수를 증가시키고 버튼 크기를 감소시킨다. 폼 제목에 점수도 같이 표시해준다.

--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,5 @@
+using System.Drawing.Text;
+
 namespace CatchButton
 {
     public partial class Form1 : Form
@@ -5,7 +7,10 @@ namespace CatchButton
         public Form1()
         {
             InitializeComponent();
+            
         }
+        //현재 점수 저장
+        private int score = 1000;
 
         private void moving_button_MouseEnter(object sender, EventArgs e)
         {
@@ -17,14 +22,25 @@ namespace CatchButton
             int nextX = rd.Next(0, maxX); int nextY = rd.Next(0, maxY);
             // 4. 위치할당(새로운Point 객체생성)
             moving_button.Location = new Point(nextX, nextY);
-            // 5. 시각적피드백(폼제목표시줄에좌표출력)
-            this.Text = $"버튼위치: ({nextX}, {nextY})";
+            // 5. 시각적피드백(폼제목표시줄에좌표출력,점수 표시)
+            this.Text = $"점수: {score} - 버튼위치: ({nextX}, {nextY})";
             // 6. 버튼이 도망갈 때 비프음 재생
             System.Media.SystemSounds.Beep.Play();
+            // 7. 점수 감소 
+            score -= 10;
         }
 
         private void moving_button_Click(object sender, EventArgs e)
         {
+            // 8. 점수 증가
+
+            score += 100;
+            // 9. 버튼 크기 감소 
+            int newW = Math.Max(30, (int)(moving_button.Width * 0.9));
+            int newH = Math.Max(20, (int)(moving_button.Height * 0.9));
+            moving_button.Size = new Size(newW, newH);
+            //점수판 갱신
+            this.Text = $"점수: {score}";
             // 버튼을 클릭했을 때 비프음 재생과 동시에 축하 메세지 출력
             System.Media.SystemSounds.Asterisk.Play();
             MessageBox.Show("축하합니다~!","성공", MessageBoxButtons.OK,MessageBoxIcon.Information);
