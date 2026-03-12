55cf24ca0904460c454bda5615b217c876d90e98
이기주 <leegiju012@gmail.com>
2026-03-12 12:03:28 +0900

    1-1 움직이는 버튼 사이즈 수

--- Form1.cs
+++ Form1.cs
@@ -12,7 +12,7 @@ namespace CatchButton
             // 1.난수생성기준비
             Random rd = new Random();
             // 2. 가용영역계산(버튼이폼테두리에걸리지않게보호) ClientSize는타이틀바와테두리를제외한실제흰도화지영역임
-            int maxX = this.ClientSize.Width - 301; int maxY = this.ClientSize.Height - 121;
+            int maxX = this.ClientSize.Width - moving_button.Width; int maxY = this.ClientSize.Height - moving_button.Width;
             // 3. 랜덤좌표추출(0 ~ 최대가용치사이)
             int nextX = rd.Next(0, maxX); int nextY = rd.Next(0, maxY);
             // 4. 위치할당(새로운Point 객체생성)
