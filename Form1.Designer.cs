8eec7f56131278f0abbfb4cce25648db69091990
이기주 <leegiju012@gmail.com>
2026-03-12 11:49:52 +0900

    1단계 과제: 난수 생성기 준비 후 가용영역 계산 후 랜던좌표를 추출해 위치를 할당하고 폼 표시줄에 좌표를 출력했다.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            moving_button = new Button();
+            SuspendLayout();
+            // 
+            // moving_button
+            // 
+            moving_button.BackColor = Color.Red;
+            moving_button.Font = new Font("휴먼둥근헤드라인", 22.2F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            moving_button.ForeColor = Color.Yellow;
+            moving_button.Location = new Point(170, 103);
+            moving_button.Name = "moving_button";
+            moving_button.Size = new Size(301, 121);
+            moving_button.TabIndex = 0;
+            moving_button.Text = "나를 잡아봐";
+            moving_button.UseVisualStyleBackColor = false;
+            moving_button.Click += moving_button_Click;
+            moving_button.MouseEnter += moving_button_MouseEnter;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(moving_button);
+            Name = "Form1";
             Text = "Form1";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button moving_button;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,25 @@ namespace CatchButton
         {
             InitializeComponent();
         }
+
+        private void moving_button_MouseEnter(object sender, EventArgs e)
+        {
+            // 1.난수생성기준비
+            Random rd = new Random();
+            // 2. 가용영역계산(버튼이폼테두리에걸리지않게보호) ClientSize는타이틀바와테두리를제외한실제흰도화지영역임
+            int maxX = this.ClientSize.Width - 301; int maxY = this.ClientSize.Height - 121;
+            // 3. 랜덤좌표추출(0 ~ 최대가용치사이)
+            int nextX = rd.Next(0, maxX); int nextY = rd.Next(0, maxY);
+            // 4. 위치할당(새로운Point 객체생성)
+            moving_button.Location = new Point(nextX, nextY);
+            // 5. 시각적피드백(폼제목표시줄에좌표출력)
+            this.Text = $"버튼위치: ({nextX}, {nextY})";
+            ;
+        }
+
+        private void moving_button_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
