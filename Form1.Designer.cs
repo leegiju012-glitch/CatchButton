9748fe88624d69101c64fdf71c899aefc1804774
이기주 <leegiju012@gmail.com>
2026-03-12 13:08:40 +0900

    4단계 과제 수정: 초기 버튼 상태와 난수 재사용을 위해 class 창을 수정하였고 misscount가 20회가 된다면 모든 버튼을 사용 안되게 하고 게임오버 메세지창을 생성한다. 그리고 다시시작 버튼을 누르면 점수, 놓친 횟수, 버튼 크기, 위치를 초기화하고 버튼을 다시 활성화한다.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             moving_button = new Button();
+            reset_button = new Button();
             SuspendLayout();
             // 
             // moving_button
@@ -45,11 +46,23 @@
             moving_button.Click += moving_button_Click;
             moving_button.MouseEnter += moving_button_MouseEnter;
             // 
+            // reset_button
+            // 
+            reset_button.Font = new Font("맑은 고딕", 15F);
+            reset_button.Location = new Point(643, 396);
+            reset_button.Name = "reset_button";
+            reset_button.Size = new Size(145, 42);
+            reset_button.TabIndex = 1;
+            reset_button.Text = "다시 시작";
+            reset_button.UseVisualStyleBackColor = true;
+            reset_button.Click += reset_button_Click;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(9F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(reset_button);
             Controls.Add(moving_button);
             Name = "Form1";
             Text = "Form1";
@@ -59,5 +72,6 @@
         #endregion
 
         private Button moving_button;
+        private Button reset_button;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -7,27 +7,55 @@ namespace CatchButton
         public Form1()
         {
             InitializeComponent();
-            
+
+            // 초기 버튼 상태와 난수 생성기 초기화
+            initialButtonSize = moving_button.Size;
+            initialButtonLocation = moving_button.Location;
+            rd = new Random();
         }
         //현재 점수 저장
         private int score = 1000;
 
+        // 난수 재사용을 위한 필드
+        private Random rd;
+
+        //버튼 놓친 횟수
+        private int missCount = 0;
+
+        //버튼 놓칠 수 있는 최대횟수
+        private const int MAX_MISSES = 20;
+
+        //초기 버튼 상태
+        private Size initialButtonSize;
+        private Point initialButtonLocation;
+
+
         private void moving_button_MouseEnter(object sender, EventArgs e)
         {
             // 1.난수생성기준비
-            Random rd = new Random();
+            //Random rd = new Random();
             // 2. 가용영역계산(버튼이폼테두리에걸리지않게보호) ClientSize는타이틀바와테두리를제외한실제흰도화지영역임
             int maxX = this.ClientSize.Width - moving_button.Width; int maxY = this.ClientSize.Height - moving_button.Height;
             // 3. 랜덤좌표추출(0 ~ 최대가용치사이)
             int nextX = rd.Next(0, maxX); int nextY = rd.Next(0, maxY);
             // 4. 위치할당(새로운Point 객체생성)
             moving_button.Location = new Point(nextX, nextY);
-            // 5. 시각적피드백(폼제목표시줄에좌표출력,점수 표시)
-            this.Text = $"점수: {score} - 버튼위치: ({nextX}, {nextY})";
-            // 6. 버튼이 도망갈 때 비프음 재생
+            // 5. 버튼이 도망갈 때 비프음 재생
             System.Media.SystemSounds.Beep.Play();
-            // 7. 점수 감소 
-            score -= 10;
+            // 6. 점수 감소 
+            score = Math.Max(0, score - 10);
+            //MissCount 증가
+            missCount++;
+            // 7. 시각적피드백(폼제목표시줄에좌표출력,점수 표시)
+            this.Text = $"점수: {score} - 버튼위치: ({nextX}, {nextY}) - 놓친횟수: {missCount}";
+            //게임 오버 
+            if (missCount >= MAX_MISSES)
+            {
+                MessageBox.Show("Game Over", "게임 종료");
+
+                // 버튼 비활성화
+                moving_button.Enabled = false;
+            }
         }
 
         private void moving_button_Click(object sender, EventArgs e)
@@ -43,7 +71,28 @@ namespace CatchButton
             this.Text = $"점수: {score}";
             // 버튼을 클릭했을 때 비프음 재생과 동시에 축하 메세지 출력
             System.Media.SystemSounds.Asterisk.Play();
-            MessageBox.Show("축하합니다~!","성공", MessageBoxButtons.OK,MessageBoxIcon.Information);
+            MessageBox.Show("축하합니다~!", "성공", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void reset_button_Click(object sender, EventArgs e)
+        {
+            // 점수 초기화
+            score = 1000;
+
+            // 놓친 횟수 초기화
+            missCount = 0;
+
+            // 버튼 크기 초기화
+            moving_button.Size = initialButtonSize;
+
+            // 버튼 위치 초기화
+            moving_button.Location = initialButtonLocation;
+
+            // 버튼 다시 활성화
+            moving_button.Enabled = true;
+
+            // 제목 초기화
+            this.Text = "Catch Button Game";
         }
     }
 }
