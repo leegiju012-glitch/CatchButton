2519d54b25ef41db4dda9f2b066a7bf3ce344a90
이기주 <leegiju012@gmail.com>
2026-03-12 12:16:00 +0900

    2단계 과제: 버튼이 도망갈때와 버튼을 클릭했을 때 비프음을 재생하고 버튼을 클릭했을 때 축하메세지 출

--- Form1.cs
+++ Form1.cs
@@ -12,19 +12,22 @@ namespace CatchButton
             // 1.난수생성기준비
             Random rd = new Random();
             // 2. 가용영역계산(버튼이폼테두리에걸리지않게보호) ClientSize는타이틀바와테두리를제외한실제흰도화지영역임
-            int maxX = this.ClientSize.Width - moving_button.Width; int maxY = this.ClientSize.Height - moving_button.Width;
+            int maxX = this.ClientSize.Width - moving_button.Width; int maxY = this.ClientSize.Height - moving_button.Height;
             // 3. 랜덤좌표추출(0 ~ 최대가용치사이)
             int nextX = rd.Next(0, maxX); int nextY = rd.Next(0, maxY);
             // 4. 위치할당(새로운Point 객체생성)
             moving_button.Location = new Point(nextX, nextY);
             // 5. 시각적피드백(폼제목표시줄에좌표출력)
             this.Text = $"버튼위치: ({nextX}, {nextY})";
-            ;
+            // 6. 버튼이 도망갈 때 비프음 재생
+            System.Media.SystemSounds.Beep.Play();
         }
 
         private void moving_button_Click(object sender, EventArgs e)
         {
-
+            // 버튼을 클릭했을 때 비프음 재생과 동시에 축하 메세지 출력
+            System.Media.SystemSounds.Asterisk.Play();
+            MessageBox.Show("축하합니다~!","성공", MessageBoxButtons.OK,MessageBoxIcon.Information);
         }
     }
 }
